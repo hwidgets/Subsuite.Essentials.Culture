f2cb601c7afe8e8c13a0fd9895cad56230f690bf
Pierre Viara <viara@intechinfo.fr>
2019-03-08 17:47:56 +0100

    Added CultureTable & its stored procedures.

---
+++ SEC/SEC.Data/CultureTable.cs
@@ -0,0 +1,47 @@
+﻿using CK.Setup;
+using CK.SqlServer;
+using CK.SqlServer.Setup;
+using System.Threading.Tasks;
+
+namespace SEC.Data
+{
+    /// <summary>
+    /// This table centralizes every single <see cref="ICulture"/>.
+    /// </summary>
+    [SqlTable("tCulture", Package = typeof(Package))]
+    [Versions("1.0.0")]
+    public abstract class CultureTable : SqlTable
+    {
+        void StObjConstruct()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ICulture"/>.
+        /// </summary>
+        /// <param name="ctx">Current <see cref="ISqlCallContext"/>.</param>
+        /// <param name="actorId">Creator's id.</param>
+        /// <param name="languageCode">See <see cref="Language.Code"/>.</param>
+        /// <returns>An <see cref="int"/> which is the created-element id.</returns>
+        [SqlProcedure("sCultureCreate")]
+        public abstract Task<int> CreateCultureAsync(
+            ISqlCallContext ctx,
+            int actorId,
+            int languageCode
+        );
+
+        /// <summary>
+        /// Deletes an <see cref="ICulture"/>.
+        /// </summary>
+        /// <param name="ctx">Current <see cref="ISqlCallContext"/>.</param>
+        /// <param name="actorId">Deletor's id.</param>
+        /// <param name="cultureId">See <see cref="ICulture.CultureId"/>.</param>
+        /// <returns></returns>
+        [SqlProcedure("sCultureDelete")]
+        public abstract Task<bool> DeleteCultureAsync(
+            ISqlCallContext ctx,
+            int actorId,
+            int cultureId
+        );
+    }
+}
